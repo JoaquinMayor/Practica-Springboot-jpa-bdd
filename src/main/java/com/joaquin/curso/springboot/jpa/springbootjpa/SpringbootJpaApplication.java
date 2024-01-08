706952ac63e42faa2668151760f5500fa97b5fd4
JoaquinMayor <joaquin88mayor@gmail.com>
2024-01-07 22:54:54 -0300

    "Manejo de bd"

--- src/main/java/com/joaquin/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/joaquin/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -1,11 +1,14 @@
 package com.joaquin.curso.springboot.jpa.springbootjpa;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.joaquin.curso.springboot.jpa.springbootjpa.entities.Person;
 import com.joaquin.curso.springboot.jpa.springbootjpa.repositories.IPersonRepository;
@@ -23,9 +26,103 @@ public class SpringbootJpaApplication implements CommandLineRunner{ //se impleme
 	@Override
 	public void run(String... args) throws Exception { //se trabaja con las instancias y probar lo que hacemos
 		
-		List<Person> persons = (List<Person>)repository.findAll();
+		create();
+	}
+
+	@Transactional //Se usa siempre que se maneja las bdd que altere la tabla o consultas
+	public void create(){
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el nombre");
+		String name = scanner.next();
+		System.out.println("Ingrese el apellido");
+		String lastname = scanner.next();
+		System.out.println("Ingrese el lenguaje de programacion");
+		String programmingLanguage = scanner.next();
+		scanner.close();
+
+		Person person = new Person(null, name, lastname, programmingLanguage);
+
+		Person personNew = repository.save(person); //guarda una nueva entidad en la base de datos
+
+		repository.findById(personNew.getId()).ifPresent(p -> System.out.println(p));
+		
+	}
+
+	@Transactional
+	public void update(){
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el id de la persona a editar");
+		Long id = scanner.nextLong();
+
+		Optional<Person> optionalPerson = repository.findById(id);
+		
+			if(optionalPerson.isPresent()){
+				Person person = optionalPerson.get();
+				System.out.println("Ingrese el lenguaje de programacion");
+				String programmingLenguage = scanner.next();
+				person.setProgrammingLanguage(programmingLenguage);
+				repository.save(person);
+			}else{
+				System.out.println("El usuario con esa id no existe");
+			}
+			
+		
+		
+		scanner.close();
+	}
+
+	@Transactional
+	public void delete(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el id de la persona a eliminar");
+		Long id = scanner.nextLong();
+		repository.deleteById(id);
+
+		
+		scanner.close();
+	}
+
+	@Transactional
+	public void delete2(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el id de la persona a eliminar");
+		Long id = scanner.nextLong();
+		
+		Optional<Person> optionalPerson = repository.findById(id);
+
+		optionalPerson.ifPresentOrElse(person -> repository.delete(person), 
+									  ()->System.out.println("El usuario no existia"));
+
+		
+		scanner.close();
+	}
+
+	@Transactional(readOnly = true)
+	public void findOne(){
+		Person person = null;
+		//Optional<Person> optionalPerson = repository.findById(8L);
+		//Optional<Person> optionalPerson = repository.findOne(2L);
+		Optional<Person> optionalPerson = repository.findByNameContaining("jo");
+		 if(optionalPerson.isPresent()){
+			person = optionalPerson.get();
+		 } //el  findById devuelve un pocional entonces tenemos que llamar al metodo get() o el orElseThow
+		
+		 System.out.println(person);
+	}
+
+	@Transactional(readOnly = true)
+	public void list(){
+		//List<Person> persons = (List<Person>)repository.findAll();
+		//List<Person> persons = (List<Person>)repository.findByProgrammingLanguage("java");
+		//List<Person> persons = (List<Person>)repository.buscarByProgrammingLanguage("Pyton", "Dario");
+		List<Person> persons = (List<Person>)repository.findByProgrammingLanguageAndName("java","joaquin");
 
 		persons.stream().forEach(person -> System.out.println(person.toString()));
+
+		List<Object[]> personsValues = repository.obtenerPersonData();
+		personsValues.stream().forEach(person -> System.out.println(person[0] + " es experto en " + person[1]));
 	}
 
 }
--- src/main/java/com/joaquin/curso/springboot/jpa/springbootjpa/repositories/IPersonRepository.java
+++ src/main/java/com/joaquin/curso/springboot/jpa/springbootjpa/repositories/IPersonRepository.java
@@ -1,10 +1,34 @@
 package com.joaquin.curso.springboot.jpa.springbootjpa.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.joaquin.curso.springboot.jpa.springbootjpa.entities.Person;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface IPersonRepository extends CrudRepository<Person, Long> { //se pone el tipo de la tabla que vamos a manejar, y el tipo de dato de la clave primaria
 
-    
+    List<Person> findByProgrammingLanguage(String programmingLanguage); //es para hacer otro filtro no es necesario agregar logica con poner el nombre de esta manera es suficiente el findBy es una palabra clave
+
+    @Query("select p from Person p where p.programmingLanguage=?1 and p.name=?2") //se pone =?1 para que sea igual al primer parametro, y si se pone un segundo se pone and parametro=?2
+    List<Person> buscarByProgrammingLanguage(String programmingLanguage, String name); //de evsta manera si no se usa la palabra clave, cuando lo ejecutamos tenemos que ponerle la anotacion @Query y la consulta no tiene que se de bdd sino de jpa
+    List<Person> findByProgrammingLanguageAndName(String programmingLanguage, String name);
+
+    @Query("select p.name, p.programmingLanguage from Person p")
+    List<Object[]> obtenerPersonData();
+
+    @Query("select p from Person p where p.id=?1")
+    Optional<Person> findOne(Long id);
+
+    @Query("select p from Person p where p.name=?1")
+    Optional<Person> findOneName(String name);
+
+    @Query("select p from Person p where p.name like %?1%")
+    Optional<Person> findOneLikeName(String name);
+
+    Optional<Person> findByName(String name); //Palabra clave para buscar uno es el findBy y el nombre del atributo
+
+    Optional<Person> findByNameContaining(String name);//Palabra clave para buscar uno es el findBy, el nombre del atributo y Containing para que funcione como el like
 } 
